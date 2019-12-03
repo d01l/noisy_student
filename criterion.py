ef61f25f1e565646b5d8b030f234ef9aa35e6ac6
robin <hydrojun@naver.com>
2019-12-04 00:44:58 +0900

    fix bug in  .gitignore

---
+++ criterion.py
@@ -0,0 +1,6 @@
+import torch
+import torch.nn.functional as F
+
+
+def cross_entropy_with_soft_target(pred, soft_targets):
+    return torch.mean(torch.sum(- soft_targets * F.log_softmax(pred, 1), 1))
